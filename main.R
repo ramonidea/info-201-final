e3ce0b27758606df8514316f7e628177d36e586f
Kosuke Terada <terakos@uw.edu>
2018-02-28 16:19:28 -0800

    got map points for events

--- main.R
+++ main.R
@@ -8,6 +8,8 @@ library(jsonlite)
 library(httr)
 library(stringi)
 library(stringr)
+library(plotly)
+
 
 #The Main function to do data wrangling
 
@@ -17,8 +19,6 @@ source("api.R")
 source("helper.R")
 #Source the helper method to get the city long, lat
 
-#The base URL for the APi
-base.url <- "https://app.ticketmaster.com/discovery/v2/"
 
 #get the music event
 #The latest 1000 records (200 per GET, using For loop to do 5 times)
@@ -130,3 +130,28 @@ ggplot()+
 
 # find all sport events
 
+#The base URL for the APi
+base.url <- "https://app.ticketmaster.com/discovery/v2/"
+sports.events.url <- paste0(base.url, "events.json?apikey=",api.key,
+                            '&size=200&countryCode=US&classificationName=Sports&page=1')
+sports.events.response <- GET(sports.events.url)
+sports.body.data <- fromJSON(content(sports.events.response,"text"))
+sports.result.data <- flatten(sports.body.data$`_embedded`$events)
+sports.result.data <- select(sports.result.data, name, url, dates.start.localDate, dates.start.localTime, dates.timezone, promoter.name, priceRanges)
+
+# add state and city to event data
+state.name <- c()
+city.name <- c()
+for (j in c(1:200)){
+  state.name <-c(state.name, sports.body.data$`_embedded`$events$`_embedded`$venues[[j]]$state$name)
+  city.name <- c(city.name, sports.body.data$`_embedded`$events$`_embedded`$venues[[j]]$city$name)
+}
+event.location = data.frame(state = state.name, city  = city.name)
+sports.result.data <- mutate(sports.result.data, state = event.location$state, city = event.location$city)
+
+# get US map data and merge to event data
+combined.city.name <- paste0(city.name, ",", state.name)
+map.data <- GetCityGeo(paste0(city.name, ",", state.name)) %>% mutate(state = event.location$state, city = sports.result.data$city)
+sports.result.data <- left_join(sports.result.data, map.data, by = c("state", "city"))
+
+# create map visualization. ScatterMapBox in plotly
