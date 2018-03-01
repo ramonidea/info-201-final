fa09093854aad4040a09d8b34aaff1badefa6f63
Ramon Qu <quramon@gmail.com>
2018-02-28 22:31:32 -0800

    UI and server update, now support the plotly, mapbox. fixhelper method bug

--- ui.R
+++ ui.R
@@ -1,42 +1,61 @@
 library(shiny)
 #source("main.R")
 library(shinyjs)
-
 library(plotly)
 
-
-
-
-ui <- fluidPage(
-  
-  htmlTemplate("./www/index.html",
-    title = tags$h2("INFO 201 Final Project - Data Analysis with TicketMaster API"),
-    dashboard = actionButton("dashboard_btn","Dashboard"),
-    music = actionButton("music_btn","Music Event"),
-    sport = actionButton("sport_btn","Sport Event"),
-    mainpanel = fluidRow(
-      column(12,
-               tabsetPanel(id="tabs",
-                           tabPanel("dashboard", 
-                                    tags$h3("Dash Board Page"),
-                                    plotOutput("lineplot")
-                           ),
-                           
-                           tabPanel("music",
-                                    tags$h3("Music Event Page"),
-                                    plotOutput("barchart")
-                           ),
-                           tabPanel('sport', 
-                                    tags$h3("Sport Event Page"),
-                                    plotOutput("piechart"),
-                                    dataTableOutput("datatable")
-                           )
-               )
-                       
-  
-             )
+ui <- htmlTemplate(
+  "./www/index.html",
+  title = tags$h2("INFO 201 Final Project - Data Analysis with TicketMaster API"),
+  #Three Button on the side to control the tabsetPanel
+  dashboard = actionButton("dashboard_btn", "Dashboard"),
+  #All of the data report goes to the main panel
+  mainpanel = fluidRow(column(
+    12,
+    tabsetPanel(
+      id = "tabs",
+      tabPanel(
+        "dashboard",
+        #------Dashboard--------
+        tags$h3("Dash Board Page"),
+        plotlyOutput("lineplot")
+        #-----End-------
+      ),
+      tabPanel("music",
+               tabsetPanel(
+                 id = "music_sub",
+                 tabPanel(
+                   "mus_pop",
+                   #-----Music Popularity------
+                   tags$h3("music popularity page"),
+                   plotOutput("barchart")
+                   #------End-------
+                 ),
+                 tabPanel(
+                   "mus_pri",
+                   #------Music Price Range---
+                   tags$h3("Music price range Page"),
+                   plotlyOutput("mapEx")
+                   #------end---------
+                   )
+               )),
+      tabPanel('sport',
+               tabsetPanel(
+                 id = "sport_sub",
+                 tabPanel(
+                   "sport_pop",
+                   #-----Sport Popularity------
+                   tags$h3("Sport Popularity Page"),
+                   plotOutput("piechart")
+                   #-----End------
+                 ),
+                 tabPanel(
+                   "sport_pri",
+                   #-----Sport Price------
+                   tags$h3("Sport Price Range Page"),
+                   dataTableOutput("datatable")
+                   #-----------End-------------
+                 )
+               ))
     )
-      
-  )
-  
+  ))
 )
