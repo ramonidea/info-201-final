c937acbd7f696d3e371685f4406c812b4f6f3ee6
Ramon Qu <quramon@gmail.com>
2018-03-04 11:13:48 -0800

    fix the issue, but found other solution works

--- helper.R
+++ helper.R
@@ -10,27 +10,7 @@ library(dplyr)
 source("api.R")
 
 
-#Get the geo location from the Google Map API
-#Input a vector of the city's name (preferablely using "city, state" format)
-#Return a data frame with columns(long, lat , name)
-geocode_city <- function(vec){
-  long <- c()
-  lat <- c()
-  name <- c()
-  for (i in vec){
-    base.url <- "https://maps.googleapis.com/maps/api/geocode/json?address="
-    address.url <- paste0(base.url, i,"&key=",google.key.1)
-    address.url<-str_replace_all(address.url," ","%20")
-    response <- GET(address.url)
-    body.data <- fromJSON(content(response,"text"))
-    loc <- body.data$results$geometry$location
-    long <- c(long, loc$lng[1])
-    lat <- c(lat, loc$lat[1])
-    name <- c(name, i)
-    print(paste("Geocode checking",i))
-  }
-  return(data.frame(long = long, lat = lat, city = name))
-}
+
 
 geocode_single_city <- function(i){
   base.url <- "https://maps.googleapis.com/maps/api/geocode/json?address="
@@ -46,13 +26,18 @@ geocode_single_city <- function(i){
 
 #Get the long and lat of the city
 GetCityGeo <- function(names){
-  cities.code <- read.csv("./data/cities.csv")
+  cities.code <- read.csv("./data/cities.csv", stringsAsFactors = FALSE)
   cities.code <- select(cities.code, long, lat, city)
+  cities.code <- na.omit(cities.code)
+  cities.code$city <- as.character(cities.code$city)
+  cities.code$long <- as.double(cities.code$long)
+  cities.code$lat <- as.double(cities.code$lat)
   long <- c()
   lat <- c()
   name <- c()
   f <- FALSE
   for (i in names){
+    print(paste0("Checking", i))
     #Check the local saved file to see whether it has been checked before or not.
     if(i %in% cities.code$city){
       temp <- cities.code[cities.code$city == i,]
@@ -62,10 +47,10 @@ GetCityGeo <- function(names){
     }else{
       f <- TRUE
       temp <- geocode_single_city(i)
-      long <- c(long, temp[1])
-      lat <- c(lat, temp[2])
-      name <- c(name, i)
-      cities.code[nrow(cities.code) + 1,] = c(temp[1],temp[2],i)
+      long <- as.double(c(long, temp[1]))
+      lat <- as.double(c(lat, temp[2]))
+      name <-as.character(c(name, i))
+      cities.code <- rbind(cities.code,c(temp[1],temp[2],i))
     }
   }
   if(f){
